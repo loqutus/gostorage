d88542d0495423e61c1ab66a40e78b28b9424c0a
Ruslan Gustomiasov <rgustomi@redhat.com>
2018-08-18 12:11:15 +0200

    commit

--- server.go
+++ server.go
@@ -1,53 +1,30 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
 	"net/http"
 	"fmt"
+	"bytes"
 	"strings"
 )
 
-func save_file(name string, content []byte) error {
-	err := ioutil.WriteFile("data/"+name, content, 0644)
-	if err != nil {
-		log.Fatal(err)
-		return err
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	var buf bytes.Buffer
+	file, header, err := r.FormFile(filename)
+	if err != nil{
+		fmt.Println(err)
 	}
-	return nil
-}
-
-func read_file(name string) ([]byte, error) {
-	data, err := ioutil.ReadFile("data/" + name)
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-	return data, nil
-}
-
-func upload_handler(w http.ResponseWriter, r *http.Request){
-	if r.Method == "POST"{
-		r.ParseMultipartForm(32 << 20)
-		file, header, err := r.FormFile("file")
-    		if err != nil {
-        	log.Fatal(err)
-		}
-		defer file.Close()
-		name := strings.Split(header.Filename, ".")
-
-	}
+	defer file.Close()
 
 }
 
-func download_handler(w http.ResponseWriter, r *http.Request){
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi. This is storage")
 	})
-	http.HandleFunc("/upload", upload_handler)
-	http.HandleFunc("/download", download_handler)
+	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/download", downloadHandler)
 	http.ListenAndServe(":8888", nil)
 }
